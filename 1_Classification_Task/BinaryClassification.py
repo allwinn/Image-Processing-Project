f1ee92e9f1e94cffe7a5e7236728dee944973b4b
Allwin Noble <allwinnoble@gmail.com>
2023-02-01 23:30:55 +0100

    Adding another conv2d layer

--- 1_Classification_Task/BinaryClassification.py
+++ 1_Classification_Task/BinaryClassification.py
@@ -17,45 +17,45 @@ import pandas as pd
 #from keras.models import load_model
 base_dir='1_classification'
 
-#Folder with ids and nonid images
+#Base Folder with ids and non-id images
 train_dir=os.path.join(base_dir,'Train')
 test_dir=os.path.join(base_dir,'Test')
-#id images for training
 
+#Train ids
 train_ids_yes=os.path.join(train_dir,'Ids')
 train_ids_no=os.path.join(train_dir,'Non_Ids')
 
+#Test ids
 test_ids_yes=os.path.join(test_dir,'Ids')
 test_ids_no=os.path.join(test_dir,'Non_Ids')
 
 train_datagen = ImageDataGenerator(rescale = 1.0/255,validation_split=0.2)
 train_generator = train_datagen.flow_from_directory(train_dir,
-                                                    batch_size=20,
+                                                   
+                                                    batch_size=64,
                                                     class_mode='binary',
                                                     target_size=(150, 150),
                                                     subset='training'
                                                     )
 validation_generator = train_datagen.flow_from_directory(train_dir,
-                                                    batch_size=20,
+                                                    batch_size=10,
+                                                    
                                                     class_mode='binary',
                                                     target_size=(150, 150),
                                                     subset='validation'
                                                     )
+
 model = tf.keras.models.Sequential([
-    # since Conv2D is the first layer of the neural network, we should also specify the size of the input
     tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150, 150, 3)),
-    # apply pooling
     tf.keras.layers.MaxPooling2D(2,2),
-    # and repeat the process
     tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2), 
+    tf.keras.layers.MaxPooling2D(2,2),
     tf.keras.layers.Conv2D(64, (3,3), activation='relu'), 
     tf.keras.layers.MaxPooling2D(2,2),
-    # flatten the result to feed it to the dense layer
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'), 
+    tf.keras.layers.MaxPooling2D(2,2),
     tf.keras.layers.Flatten(), 
-    # and define 512 neurons for processing the output coming by the previous layers
     tf.keras.layers.Dense(512, activation='relu'), 
-    # a single output neuron. The result will be 0 if the image is a cat, 1 if it is a dog
     tf.keras.layers.Dense(1, activation='sigmoid')  
 ])
 
@@ -65,53 +65,50 @@ model.compile(optimizer="adam",
               metrics = ['accuracy'])
 
 
+history = model.fit(
+            train_generator, # pass in the training generator
+            epochs=10,
+            steps_per_epoch=10,
+            validation_data=validation_generator, # pass in the validation generator
+            verbose=2
+            )
+
+
+model.save_weights("modelv2_colour_4conv.h5")
+
+
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 
 
-
-history = model.fit(
-            train_generator, # pass in the training generator
-            steps_per_epoch=10,
-            epochs=10,
-            validation_data=validation_generator, # pass in the validation generator
-            validation_steps=5,
-            verbose=2
-            )
-
-
-model.save_weights("modelv2.h5")
-
-
-
-
 #Displaying output
 test_filenames=[]
 path = [test_ids_yes, test_ids_no]
+
 for i in path:
     test_filenames.append(os.listdir(i))
     
+    
 test_df = pd.DataFrame({
-    'filename': test_filenames[0]+test_filenames[1]
-})
+    'filename': test_filenames[0]+test_filenames[1]})
 nb_samples = test_df.shape[0]
 
 test_datagen = ImageDataGenerator(rescale = 1.0/255)
 test_generator = train_datagen.flow_from_directory(test_dir,
-                                                    batch_size=20,
                                                     class_mode=None,
                                                     target_size=(150, 150),
                                                     shuffle=False
                                                     )
 
 
-model.load_weights('modelv1.h5')
+model.load_weights('modelv2_grayscale_4conv.h5')
 #new_model=tf.keras.models.load_model('modelv1.h5')
-predict = model.predict(test_generator, steps=np.ceil(nb_samples/20))
+predict = model.predict(test_generator)
 
-test_df['id_present'] = [0 if x<=0.5 else 1 for x in predict]
+test_df['id_present'] = [0 if x<=0.7 else 1 for x in predict]
+#test_df['id_present'] = [x for x in predict]
 test_df.to_csv("labels_test.csv")
 
 
@@ -133,3 +130,13 @@ plt.figure()
 plt.plot(epochs, loss)
 plt.plot(epochs, val_loss)
 plt.title('Loss in training and validation')
+
+#%matplotlib inline
+
+#view images wrongly classified
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+img = mpimg.imread('./1_classification/Test/Non_Ids/n03792782_276.png')
+plt.imshow(img)
+
+
